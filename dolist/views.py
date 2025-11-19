5e6ff0f3a00b2f4f44693f217c7b03c9fc08024c
Jxsephhhh <joseph.bernabe89@login.cuny.edu>
2025-11-19 16:42:28 +0000

    wednesday

--- dolist/views.py
+++ dolist/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-from . models import Todolist
+from django.shortcuts import render, redirect
+from .models import Todolist
+from .forms import Todolisform
+from django.views.decorators.http import require_POST
+
 # Create your views here.
 def index(request):
-    todo_items = Todolist.objects.order_by('id')
-    context = {'todo_items' : todo_items} 
+    todo_tasks = Todolist.objects.order_by('id')
+    form = Todolisform()
+    context = {'todo_tasks' : todo_tasks, 'form' : form} 
     return render(request, 'index.html', context)
+
+@require_POST
+def addTodoitem(request):
+    form = Todolisform(request.POST)
+
+
+    # capture data from the form when the Add to list button is pressed
+    if form.is_valid():
+        form.save()
+
+
+    return redirect('index')
